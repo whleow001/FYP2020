80575bbfc9858648c5661fc945c1d6a4381fd3c3
noobist <yong1994@live.com>
2020-09-12 21:25:41 +0800

    WebGL build for fog of war shader

--- Assets/Scripts/PhotonV2/Utilities/Network/GameDirector.cs
+++ Assets/Scripts/PhotonV2/Utilities/Network/GameDirector.cs
@@ -10,14 +10,26 @@ public class GameDirector : MonoBehaviourPun
     [SerializeField]
     private GameObject _mainCamera;
     [SerializeField]
+    private GameObject fovMask;
+    [SerializeField]
     private List<GameObject> spawns = new List<GameObject>();
     [SerializeField]
     private List<GameObject> prefabs = new List<GameObject>();
 
-    private void Awake() {
-      int index = (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] - 1;
+    // Team no
+    private int team;
 
-      GameObject playerClone = MasterManager.NetworkInstantiate(prefabs[index], spawns[index].transform.GetChild(Random.Range(0, 3)).transform.position, Quaternion.identity);
+    // Layer references
+    private int GOVT_LAYER = 9;
+    private int REBEL_LAYER = 10;
+
+    // flags
+    public bool maskSet = false;
+
+    private void Awake() {
+      team = (int)PhotonNetwork.LocalPlayer.CustomProperties["Team"] - 1;
+
+      GameObject playerClone = MasterManager.NetworkInstantiate(prefabs[team], spawns[team].transform.GetChild(Random.Range(0, 3)).transform.position, Quaternion.identity);
       playerClone.GetComponent<PlayerController>().SpawnCamera(_mainCamera);
 
       // If client is a master client
@@ -34,6 +46,42 @@ public class GameDirector : MonoBehaviourPun
         for (int i = 0; i < randomIndexes.Count; i++)
           MasterManager.RoomObjectInstantiate(prefabs[2], spawns[2].transform.GetChild(randomIndexes[i]).transform.position, Quaternion.identity);
       }
+    }
+
+    void Update() {
+      if (!maskSet)
+        AllocateFOVMask();
+    }
+
+    private void AllocateFOVMask() {
+      // Get all players
+      GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+      foreach (GameObject player in allPlayers)
+        if (player.layer == GetFactionLayer())
+            AddMaskAsChild(player);
+
+      // Get all generators
+      if (team == 1) {
+        GameObject[] generators = GameObject.FindGameObjectsWithTag("Generator");
+
+        foreach (GameObject generator in generators) {
+          AddMaskAsChild(generator);
+        }
+      }
+
+
+
+      maskSet = true;
+    }
+
+    private void AddMaskAsChild(GameObject _gameObject) {
+      Debug.Log(_gameObject);
+      GameObject FOVObject = Instantiate(fovMask, new Vector3(_gameObject.transform.position.x, _gameObject.transform.position.y + 0.03f, _gameObject.transform.position.z), Quaternion.identity);
+      FOVObject.transform.SetParent(_gameObject.transform);
+    }
+
+    private int GetFactionLayer() {
+      return team == 0 ? GOVT_LAYER : REBEL_LAYER;
     }
 
     public void AddToCombatLog(PhotonView victimID, PhotonView killerID) {
