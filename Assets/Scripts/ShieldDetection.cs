97c751170d99c59ad8f2a7cd3be7439b14a2cb79
whleow001 <68463156+whleow001@users.noreply.github.com>
2020-11-14 18:06:07 +0800

    fix attempt for jugg shield, HELP PLS

--- Assets/Scripts/ShieldDetection.cs
+++ Assets/Scripts/ShieldDetection.cs
@@ -9,14 +9,16 @@ public class ShieldDetection : MonoBehaviour
     private void Awake()
     {
         //gameObject.layer = transform.parent.gameObject.layer;
+        Physics.IgnoreLayerCollision(18, gameObject.transform.parent.gameObject.layer);
+        Physics.IgnoreLayerCollision(18, 17);
     }
 
     private void OnEnable()
     {
         Debug.Log(gameObject.transform.parent.gameObject.layer);
         Debug.Log(gameObject.layer);
-        Physics.IgnoreLayerCollision(18, gameObject.transform.parent.gameObject.layer);
-        Physics.IgnoreLayerCollision(18, 17);
+        //Physics.IgnoreLayerCollision(18, gameObject.transform.parent.gameObject.layer);
+        //Physics.IgnoreLayerCollision(18, 17);
     }
 
     private void OnCollisionEnter(Collision collision)
