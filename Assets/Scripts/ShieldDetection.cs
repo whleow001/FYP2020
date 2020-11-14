8a8e6029ed36141d14ecf17ca01b9a7e7f51f77e
whleow001 <68463156+whleow001@users.noreply.github.com>
2020-11-14 17:21:48 +0800

    fix attempt for jugg shield, lost count of attempt

--- Assets/Scripts/ShieldDetection.cs
+++ Assets/Scripts/ShieldDetection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,14 +8,26 @@ public class ShieldDetection : MonoBehaviour
 
     private void Awake()
     {
-        gameObject.layer = transform.parent.gameObject.layer;
+        //gameObject.layer = transform.parent.gameObject.layer;
     }
 
     private void OnEnable()
     {
         Debug.Log(gameObject.transform.parent.gameObject.layer);
         Debug.Log(gameObject.layer);
-        //Physics.IgnoreLayerCollision(18, gameObject.transform.parent.gameObject.layer);
-        //Physics.IgnoreLayerCollision(18, 17);
+        Physics.IgnoreLayerCollision(18, gameObject.transform.parent.gameObject.layer);
+        Physics.IgnoreLayerCollision(18, 17);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+        if (transform.parent.gameObject.GetComponent<PhotonView>().IsMine)
+        {
+            if (collision.gameObject.tag == "Projectile" && collision.gameObject.layer == transform.parent.gameObject.GetComponent<PlayerContainer>().GetPlayerManager().GetDirector().GetOtherFactionLayer())
+            {
+                PhotonNetwork.Destroy(collision.gameObject);
+            }
+        }
     }
 }
